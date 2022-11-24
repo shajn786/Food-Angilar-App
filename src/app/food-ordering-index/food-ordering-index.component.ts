e35b081a15803392ee2940e982605f2fae755570
shajn786 <shajn786@gmail.com>
2022-11-24 23:41:55 +0530

    index

---
+++ src/app/food-ordering-index/food-ordering-index.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-food-ordering-index',
+  templateUrl: './food-ordering-index.component.html',
+  styleUrls: ['./food-ordering-index.component.css']
+})
+export class FoodOrderingIndexComponent {
+
+}
