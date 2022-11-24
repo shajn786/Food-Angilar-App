02b9c723dbb27f2e11f693a846f39a0555d39f6c
shajn786 <shajn786@gmail.com>
2022-11-25 00:51:30 +0530

    nav bar

---
+++ src/app/food-navbar/food-navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-food-navbar',
+  templateUrl: './food-navbar.component.html',
+  styleUrls: ['./food-navbar.component.css']
+})
+export class FoodNavbarComponent {
+
+}
